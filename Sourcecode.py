0145e20653191c80a5c41ff6d59a9aa0fcfba235
Sai Kumar Devangula <91788659+DEVANGULASAIKUMAR@users.noreply.github.com>
2023-02-04 16:57:48 +0530

    Update Sourcecode.py

--- Sourcecode.py
+++ Sourcecode.py
@@ -1,2 +1,14 @@
 # Desktopnotification-Project
 
+import time
+from plyer import notification
+if __name__ == '__main__':
+    while True:
+        notification.notify(
+            title = "All Day Reminder",
+            message = "Today I have to Submit my assignment and you have "
+            "not completed it till now",
+            app.icon = icon.ico.htm,
+            timeout = 5
+        )
+        time.sleep(60*60)
